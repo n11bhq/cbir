bd0b63d1cef9fede12e3f4b32b216992c77904c2
miracle <79822729+n11bhq@users.noreply.github.com>
2022-11-19 19:00:33 +0700

    update query

--- feature_extractor.py
+++ feature_extractor.py
@@ -6,15 +6,20 @@ import numpy as np
 class FeatureExtractor:
     def __init__(self):
 
-        self.base_model = InceptionV3(weights="imagenet")
-        self.model = Model(inputs=self.base_model.input, outputs=self.base_model.get_layer("avg_pool").output)
+        self.base_model = InceptionV3(
+                                    weights="imagenet"
+                                    )
+        self.model = Model(
+                        inputs=self.base_model.input, 
+                        outputs=self.base_model.get_layer("avg_pool").output
+                        )
      
 
     def extract(self, img):
         height = 299
         width = 299
         img = img.resize((height, width)).convert("RGB")
-        x = image.img_to_array(img)  # to np.array
+        x = image.img_to_array(img)  # convert to array
         x = np.expand_dims(x, axis=0)  # (H, W, C) -> (1, H, W, C)
         feature = self.model.predict(x)[0]  # (1, 4096) -> (4096)
         return feature/np.linalg.norm(feature)  # normalize
